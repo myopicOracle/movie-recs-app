c56ecc092ab0cb66c8205fa975c10b5bd0ca98d0
Gary Xia (myopicOracle) <184602237+myopicOracle@users.noreply.github.com>
2025-11-04 16:12:04 -0800

    init react project and replace vite templates

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>My React App</h1>
+      <div className="card">
+
+      </div>
+      <p className="read-the-docs">
+        <a href="https://github.com/myopicOracle" target="_blank" rel="noopener noreferrer">
+          @myopicOracle
+        </a>
+      </p>
+    </>
+  )
+}
+
+export default App
