c013e7e387968f2619889e83009849ba6e628a5d
Gary Xia (myopicOracle) <184602237+myopicOracle@users.noreply.github.com>
2025-11-05 23:43:44 -0800

    chore: add supabase endpoint config module (#5)

---
+++ config.js
@@ -0,0 +1,6 @@
+import 'dotenv/config'
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://ynaholibienybgjtnzcu.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+export const supabase = createClient(supabaseUrl, supabaseKey)
