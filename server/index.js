97ad601d5ea9ab0ffd146c43335ccfd42a2dbf51
Gary Xia (myopicOracle) <184602237+myopicOracle@users.noreply.github.com>
2025-11-09 23:29:17 -0800

    feat: write and test Supabase semantic search function (closes #12)

--- server/index.js
+++ server/index.js
@@ -1,6 +1,25 @@
 import { openai, supabase } from './config.js'
 import { createEmbedding } from './services/embeddingService.js'
 
-const query = "Fly me to the moon."
-const embedding = await createEmbedding(query)
-console.log(embedding)
+
+async function main() {
+    const query = 'I want to watch an action movie.'
+    await semanticSearch(query)
+}
+main()
+
+
+async function semanticSearch(input) {
+    const embedding = await createEmbedding(input)
+    console.log('Embedding successfully returned. Embedding: ', embedding)
+
+    // https://supabase.com/docs/guides/ai/vector-columns
+    const { data, error } = await supabase.rpc('match_documents', {
+      query_embedding: embedding, // Pass the embedding you want to compare
+      match_threshold: 0.1, // Choose an appropriate threshold for your data
+      match_count: 3, // Choose the number of matches
+    })
+    
+    if (error) console.error('Error: ', error)
+    console.log('Search Result: ', data)
+}
